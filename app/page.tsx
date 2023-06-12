f8e6adf674d28d5bde2fb6dea34f6703bd21393f
hassanakram-n <hassanakram.n@gmail.com>
2023-06-12 12:03:56 +0500

    Next.js TypeScript Starter

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Home: React.FC = () => {
+  return (
+    <main className="grid h-screen place-content-center">
+      <h1 className="">Hello World</h1>
+    </main>
+  )
+}
+
+export default Home
